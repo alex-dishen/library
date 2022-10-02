2335e5314ddf9541fed85fb8a59ef534aaedaed6
Alex <didyshen.oleksandr@gmail.com>
2022-10-02 10:08:26 +0300

    Create add book form

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 const books = document.querySelectorAll('.book');
+const addBookBtn = document.querySelector('.add-book');
+const overlay = document.querySelector('.overlay');
+const modal = document.querySelector('.modal');
+const checkBox = document.getElementById('checkbox');
+const ratingOption = document.querySelector('.rating');
+const rateValue = document.getElementById('rate-value');
+const starContainer = document.querySelector('.stars');
+const starOverlay = document.querySelector('.star-overlay')
 
-function showBookStatus(book) {
+function showBookForm() {
+    overlay.style.display = 'block';
+    modal.classList.add('active');
+    overlay.addEventListener('click', removeBookForm);
+}
+
+function removeBookForm() {
+    overlay.style.display = 'none';
+    modal.classList.remove('active')
+}
+
+function changeBookStatus(book) {
     if(book.getAttribute('class') === 'book read-book') {
         book.classList.remove('read-book');
         book.classList.add('not-read-book');
@@ -12,6 +31,27 @@ function showBookStatus(book) {
     }
 }
 
+function showRatingOption() {
+    if(checkBox.checked === true) {
+        ratingOption.style.display = 'flex';
+    } else if(checkBox.checked === false) {
+        ratingOption.style.display = 'none';
+    }
+}
+
+function rating(value) {
+    const percentage = Math.round((value / 5) * 100);
+    starOverlay.style.width = `${100 - percentage}%`;
+}
+
 books.forEach((book) => {
-    book.addEventListener('click', () => {showBookStatus(book)})
- })
+    book.addEventListener('click', () => {changeBookStatus(book)})
+ });
+
+ addBookBtn.addEventListener('click', showBookForm);
+
+ rateValue.addEventListener('change', () => {
+    rating(rateValue.value);
+ });
+
+ checkBox.addEventListener('click', showRatingOption);
